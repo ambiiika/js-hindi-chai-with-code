31059a3fcc03614abf29337704e4f54702b0931d
ambiiika <166899002+ambiiika@users.noreply.github.com>
2025-01-20 16:01:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+ "use strict"; // treat all JS code as newer version
+
+ // alert(3 + 3) // we are using nodejs, not browser
+
+ console.log(3
+    + 
+     3) // code readibility should be high
+
+     console.log("hitesh") 
+
+     let name="hitesh"
+     let age =18
+     let isLoggedIn = false
+     let state = null
+
+
+     // number => 2 to power 53
+     // bigint
+     // string => ""
+     // boolean => true/false
+     // null => standalone value
+     // undefined => // value assign ni hui ha mtlb
+     // symbol => unique
+
+
+     // object 
+
+     // console.log(typeof "hitesh");
+     console.log(typeof null);
